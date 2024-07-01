11568d3838e5041d1e0e37b2f6062751af6e6808
Onurcan Bülbül <72456691+Onurcan1993@users.noreply.github.com>
2024-07-01 16:27:17 +0300

    twelve

--- src/sideComponents/side.styled.jsx
+++ src/sideComponents/side.styled.jsx
@@ -1,8 +1,8 @@
 import styled from "styled-components";
-import Logo from "../../public/vite.svg";
+// import Logo from "../../public/vite.svg";
 
 export const Container = styled.div`
-  background-image: "url('${Logo}')";
+  background-image: url("../../public/vite.svg");
   background-repeat: no-repeat;
   background-size: cover;
   background-position: bottom;
