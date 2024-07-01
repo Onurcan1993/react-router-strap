147e8f9a30082702a82735eeb6becb32390289af
Onurcan Bülbül <72456691+Onurcan1993@users.noreply.github.com>
2024-07-01 15:27:04 +0300

    side-logo-updated

--- src/sideComponents/side.styled.jsx
+++ src/sideComponents/side.styled.jsx
@@ -1,8 +1,8 @@
 import styled from "styled-components";
-import backLogo from "../assets/images/bg-sidebar-desktop.svg";
+// import backLogo from "../assets/images/bg-sidebar-desktop.svg";
 
 export const Container = styled.div`
-  background-image: url(${backLogo});
+  background-image: url("../assets/images/bg-sidebar-desktop.svg");
   background-repeat: no-repeat;
   background-size: cover;
   background-position: bottom;
